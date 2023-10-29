8a60ec8bcba123ebe566ef2b0d5d1ea7d592bb1e
Walter Palacios <138240963+WalterCoder@users.noreply.github.com>
2023-10-29 16:23:40 -0600

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,14 @@
+const secondPart = document.querySelector('.secondPart');
+const buttonArea = document.querySelector('.button-area');
+
+// Función para mostrar la segunda parte y el botón de manera atractiva
+function mostrarElementosConEstilo() {
+    // Agrega la clase 'show' después de 2.5 segundos (2500 milisegundos)
+    setTimeout(() => {
+        secondPart.classList.add('show');
+        buttonArea.classList.add('show');
+    }, 2000);
+}
+
+// Llama a la función para mostrar los elementos
+mostrarElementosConEstilo();
